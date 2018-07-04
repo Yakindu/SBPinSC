b92eadfa30394d72edb77b7e94ba03402faea9a5
René Beckmann <rene.bckmnn@gmail.com>
2018-07-04 14:45:05 +0200

    WIP

--- examples/RocketLanding/src/com/edu4java/samplegame/Game.java
+++ examples/RocketLanding/src/com/edu4java/samplegame/Game.java
@@ -1,26 +1,33 @@
 package com.edu4java.samplegame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
-import org.yakindu.scr.gameobjects.GameObjectsStatemachine;
-import org.yakindu.scr.TimerService;
-import org.yakindu.scr.gameobjects.IGameObjectsStatemachine.SCInterfaceListener;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.geom.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import org.yakindu.scr.TimerService;
+import org.yakindu.scr.gameobjects.GameObjectsStatemachine;
+import org.yakindu.scr.gameobjects.IGameObjectsStatemachine.SCInterfaceListener;
 
 @SuppressWarnings("serial")
 
-public class Game extends JPanel implements ActionListener, KeyListener {
-	
+public class Game extends JPanel implements KeyListener, ActionListener {
+
 	private Base base;
-	private Rectangle ground;
+	private Ground ground;
 	private Wall wall1;
 	private Wall wall2;
 	private Rocket rocket;
@@ -34,105 +41,141 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
-		
+
 		this.base = new Base();
-		this.ground = new Rectangle(0, 340, 300, 30);
+		this.ground = new Ground(0, 340, 300, 30);
 		this.wall1 = new Wall(10);
 		this.wall2 = new Wall(250);
 		this.rocket = new Rocket(this.wall1.getLocation()+10, this.wall2.getLocation(), 10, 100);
 	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		ground.paint(g2d);
+		base.paint(g2d);
+		wall1.paint(g2d);
+		wall2.paint(g2d);
+		rocket.paint(g2d);
+		
+		g2d.drawString(rocket.getString(), 150, 50);
+	}
 	
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-            RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.fill(ground);
-        g2d.fill(base.getShape());
-        g2d.fill(wall1.getShape());
-        g2d.fill(wall2.getShape());
-        g2d.fill(rocket.getShape());
-        g2d.drawString(rocket.getString(), 150, 50);
-    }
-    
-    public void keyPressed(KeyEvent e){
-    	int code = e.getKeyCode();
-    	if (code == KeyEvent.VK_RIGHT){
-    		this.rocket.moveRight();
-    	}
-    	
-    	if (code == KeyEvent.VK_LEFT){
-    		this.rocket.moveLeft();
-    	}
-    	
-    	if (code == KeyEvent.VK_P){
-    		this.rocket.pause();
-    	}
-    }
-    
-    public void actionPerformed(ActionEvent e){
+	@Override
+	public void actionPerformed(ActionEvent e){
     	
     }
-    
-    public void keyTyped(KeyEvent e){}
-    
-    public void keyReleased(KeyEvent e){}
-    
-    
-	private class Base{
-		
+
+	@Override
+	public void keyPressed(KeyEvent e){
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_RIGHT){
+			this.rocket.moveRight();
+		}
+
+		if (code == KeyEvent.VK_LEFT){
+			this.rocket.moveLeft();
+		}
+
+		if (code == KeyEvent.VK_P){
+			this.rocket.pause();
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e){}
+
+	@Override
+	public void keyReleased(KeyEvent e){}
+
+
+	private class Base implements GameAsset {
+
 		Random rand;
-		
+
 		public Base(){
 			this.rand = new Random();
 		}
-		
+
 		private int location = 150;
-		
+
 		public int getLocation(){
 			return location;
 		}
-		
+
 		public Shape getShape(){
 			return new Rectangle(location, 330, 10, 10);
 		}
-		
+
 		public void moveBase(){
 			int i = rand.nextInt(40);
 			location = location - (i-20);
 		}
 		
+		public void paint(Graphics2D g2d) {
+			g2d.fill(this.getShape());
+		}
+
 	}
-	
-	
-	private class Wall{
-		
+
+
+	private class Wall implements GameAsset {
+
 		private int location;
-		
+
 		public Wall(int location){
 			this.location = location;
 		}
-		
+
 		public Shape getShape(){
 			return new Rectangle(location, 10, 10, 500);
 		}
-		
+
 		public int getLocation(){
 			return this.location;
 		}
 		
+		public void paint(Graphics2D g2d) {
+			g2d.fill(this.getShape());
+		}
+	}
+	
+	private class Ground implements GameAsset {
+		private Rectangle rect;
+		
+		public Ground(int x, int y, int width, int height) {
+			this.rect = new Rectangle(x, y, width, height);
+		}
+		
+		public Shape getShape() {
+			return this.rect;
+		}
+		
+		public void paint(Graphics2D g2d) {
+			g2d.fill(this.getShape());
+		}
 	}
 
-	private class Rocket{
+	private class Rocket implements GameAsset {
 
 		private GameObjectsStatemachine sm;
 		private String s;
-		
+		private Image image;	
+
 		public Rocket(int wall1, int wall2, int width, int height){
-			
+
 			this.s = "";
-			
+			try {
+				this.image = ImageIO.read(new File("resources/rocket.bmp"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 			this.sm = new GameObjectsStatemachine();
 			sm.setTimer(new TimerService());
 			sm.init();
@@ -146,84 +189,105 @@ public class Game extends JPanel implements ActionListener, KeyListener {
 			sm.enter();
 			sm.raiseInitSystem();
 		}
-		
+
 		public void pause() {
 			sm.raisePausePressed();
-			
 		}
 
 		SCInterfaceListener myL = new SCInterfaceListener() {
-			
+
 			@Override
 			public void onGameOverWinRaised() {
 				s = "YOU WIN!";
-				
+
 			}
-			
+
 			@Override
 			public void onGameOverLoseRaised() {
 				s = "YOU LOSE!";
-				
+
 			}
-				
+
 			@Override
 			public void onDisplayPauseMessageRaised() {
 				s = "Game Paused";
-				
+
 			}
 
 			@Override
 			public void onDeletePauseMessageRaised() {
 				s = "";
-				
+
 			}
-
-
-			
 		};
-		
-		
+
 		public String getString() {
 			return s;
 		}
 
 		public void moveLeft() {
 			sm.raiseLeftButtonPressed();
-			
+
 		}
 
 		public void moveRight() {
 			sm.raiseRightButtonPressed();
-			
+
 		}
 
 		public void moveRocket(){
-			
+
 			sm.runCycle();
 			//System.out.println(sm.getRocketY());
 		}
-		
+
 		public Shape getShape(){
 			return new Rectangle((int)sm.getRocketX(), (int)sm.getRocketY(), 10, 100);
 		}
+		
+		public void paint(Graphics2D g2d) {
+			g2d.drawImage(this.image, 
+					getX(), getY(),
+					getX() + getWidth(), getY() + getHeight(),
+					0, 0,
+					this.image.getWidth(null), this.image.getHeight(null), 
+					null);
+		}
+		
+		private int getX() {
+			return (int)sm.getRocketX();
+		}
+		private int getY() {
+			return (int) sm.getRocketY();
+		}
+		private int getWidth() {
+			return (int) sm.getRocketWidth();
+		}
+		private int getHeight() {
+			return (int) sm.getRocketHeight();
+		}
 	}
 	
-    public static void main(String[] args) throws InterruptedException {
-        JFrame frame = new JFrame("Sample Frame");
-        Game game = new Game();
-        
-        frame.add(game);
-        frame.setSize(300, 400);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public interface GameAsset {
+		public void paint(Graphics2D g2d);
+	}
 
-        while (true) {
-        	//game.base.moveBase();
-        	game.rocket.moveRocket();
-            game.repaint();
-            Thread.sleep(100);
-        }
-    }
+	public static void main(String[] args) throws InterruptedException {
+		JFrame frame = new JFrame("Rocket Game");
+		Game game = new Game();
+
+		frame.add(game);
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		while (true) {
+			//game.base.moveBase();
+			game.rocket.moveRocket();
+			game.repaint();
+			Thread.sleep(10);
+		}
+	}
 
 
 }
